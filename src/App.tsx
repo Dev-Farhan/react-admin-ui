0f9227b7267403c385879c57b332f079178db8c9
Dev-Farhan <mohdfarhan0678@gmail.com>
2023-12-05 12:43:43 +0530

    commit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import Home from "./pages/home/Home";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Users from "./pages/users/Users.tsx";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar.tsx";
+import Menu from "./components/menu/Menu.tsx";
+import Footer from "./components/footer/Footer.tsx";
+import Login from "./pages/login/Login.tsx";
+import "./styles/global.scss";
+import User from "./pages/user/User.tsx";
+import Product from "./pages/product/Product.tsx";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+const Footer = () => {
+  return (
+    <div className="footer">
+     <span>faradmin</span>
+     <span>© Farhan Admin Dashboard</span>
+    </div>
+  )
+}
+
+
+export default Footer
